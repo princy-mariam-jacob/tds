d869abded817b6b52a850c612fa17e52dfb9fad0
princy-mariam-jacob <93709324+princy-mariam-jacob@users.noreply.github.com>
2022-07-24 22:15:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Multiplication of 2 numbers')
+st.title('division of 2 numbers')
 a = st.number_input('Enter a number')
 b = st.number_input('Enter another number')
 result = a / b

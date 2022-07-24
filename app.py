ffb88576e04035490641f2a2206bf60ffa7272bb
princy-mariam-jacob <93709324+princy-mariam-jacob@users.noreply.github.com>
2022-07-24 22:02:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Multiplication of 2 numbers')
 a = st.number_input('Enter a number')
 b = st.number_input('Enter another number')
-result = a * b
-st.write(a, ' * ', b , '= ', result)
+result = a / b
+st.write(a, ' / ', b , '= ', result)
